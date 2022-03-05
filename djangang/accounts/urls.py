5c0750d6104bb1130c3d4b8bec6a58de4c86b5de
jacobef <jacobef2@gmail.com>
2022-03-05 13:15:59 -0700

    add empty account page

--- djangang/accounts/urls.py
+++ djangang/accounts/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('login/', views.LoginView.as_view()),
+    path('my-account/', views.my_account),
 ]
--- djangang/accounts/views.py
+++ djangang/accounts/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 import django.contrib.auth.views as django_auth_views
 # Create your views here.
 
+
 def splash(request):
     return render(request, "accounts/splash.html")
+
+
+def my_account(request):
+    return render(request, "accounts/my_account.html")
+
 
 class LoginView(django_auth_views.LoginView):
     template_name = "accounts/login.html"
