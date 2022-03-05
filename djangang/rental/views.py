0b174576ab27e8a915378b18aaebf794396382e3
jacobef <jacobef2@gmail.com>
2022-03-05 15:29:27 -0700

    cleanup/nitpicks

--- djangang/rental/views.py
+++ djangang/rental/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import CreateView, DetailView, UpdateView
+from django.views.generic import CreateView, UpdateView
 
 from rental.models import Tool
 
