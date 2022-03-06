5db1cc39a90e580cbe36c0c1d0fe1feea70830ef
sast2883 <sast2883@colorado.edu>
2022-03-05 17:46:02 -0700

    Nav Bar v1

--- djangang/accounts/urls.py
+++ djangang/accounts/urls.py
@@ -1,9 +1,11 @@
 import django.contrib.auth.views as django_auth_views
 from accounts import views
 from django.urls import path
+app_name = "accounts"
+
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view()),
-    path('register/', views.UserCreateView.as_view()),
-    path('profile/', views.profile),
+    path('login/', views.LoginView.as_view(), name="login"),
+    path('register/', views.UserCreateView.as_view(), name="register"),
+    path('profile/', views.profile, name="profile"),
 ]
