887ca9a14981c6807a4b3195fea950c80eaebde0
jacobef <jacobef2@gmail.com>
2022-03-05 21:42:31 -0700

    add more parameters to tool search

--- djangang/rental/forms.py
+++ djangang/rental/forms.py
@@ -5,3 +5,6 @@ from rental.models import Tool
 
 class ToolSearchForm(forms.Form):
     by_type = forms.ChoiceField(choices=Tool.Type.choices, required=False)
+    by_name = forms.CharField(max_length=50, required=False)
+    by_brand = forms.CharField(max_length=50, required=False)
+    by_price = forms.DecimalField(decimal_places=2, max_digits=7, required=False)
