fa4d058a05425edc4c840f63bfcf6abe1de9371a
sast2883 <sast2883@colorado.edu>
2022-03-05 21:28:19 -0700

    Add Path/Page for Rent Tool

--- djangang/rental/urls.py
+++ djangang/rental/urls.py
@@ -10,4 +10,5 @@ urlpatterns = [
     path('search/results', views.tool_search_results, name="search-results"),
     path('map/', views.local_map, name="map"),
     path('tool/<int:pk>', views.ToolUpdateView.as_view()),
+    path('rent/', views.rent, name="rent"),
 ]
