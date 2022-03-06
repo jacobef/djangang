3ec828dd1661c71c370cd90bbc86982352fbe6d9
jacobef <jacobef2@gmail.com>
2022-03-05 17:37:55 -0700

    url/appname nitpicks

--- djangang/accounts/urls.py
+++ djangang/accounts/urls.py
@@ -2,6 +2,8 @@ import django.contrib.auth.views as django_auth_views
 from accounts import views
 from django.urls import path
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('login/', views.LoginView.as_view()),
     path('register/', views.UserCreateView.as_view()),
--- djangang/rental/urls.py
+++ djangang/rental/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-app_name = "rental"
+
+app_name = 'rental'
 
 urlpatterns = [
     path('', views.home),
