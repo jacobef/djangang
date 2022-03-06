377da7a5fc5b7c5b769661ede52005ef5cfea2e5
jacobef <jacobef2@gmail.com>
2022-03-05 22:42:41 -0700

    fix generate_geojson

--- djangang/generate_geojson.py
+++ djangang/generate_geojson.py
@@ -11,21 +11,21 @@ from django.db.models import QuerySet
 from rental.models import Tool
 
 
-def generate_geojson():
-    def generate_tool_geojson(tool: Tool):
-        return f"""        {{
-            "type": "Feature",
-            "properties": {{}},
-            "geometry": {{
-                "type": "Point",
-                "coordinates": [
-                    {tool.location[0]},
-                    {tool.location[1]}
-                ]
-            }}
-        }}"""
+def generate_tool_geojson(tool: Tool):
+    return f"""        {{
+        "type": "Feature",
+        "properties": {{}},
+        "geometry": {{
+            "type": "Point",
+            "coordinates": [
+                {tool.location[0]},
+                {tool.location[1]}
+            ]
+        }}
+    }}"""
 
-    def generate_map_geojson(tools: QuerySet):
+
+def generate_map_geojson(tools: QuerySet):
         features_content = ""
         for tool in tools:
             features_content += generate_tool_geojson(tool)
@@ -37,5 +37,7 @@ def generate_geojson():
     ]
 }}"""
 
+
+def generate_geojson():
     tools = Tool.objects.all()
     return generate_map_geojson(tools)
